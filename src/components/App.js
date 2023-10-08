2e78ee0ffbc6b343dbe99d99ccc351ee6136570d
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-10-08 10:56:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-
 import React, { Component } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
@@ -12,19 +11,58 @@ class App extends Component {
     };
   }
 
+  handleRestart = () => {
+    this.setState({ index: 0 });
+  };
+
+  handlePrev = () => {
+    this.setState((prevState) => ({
+      index: prevState.index > 0 ? prevState.index - 1 : 0,
+    }));
+  };
+
+  handleNext = () => {
+    this.setState((prevState) => ({
+      index:
+        prevState.index < this.state.slides.length - 1
+          ? prevState.index + 1
+          : prevState.index,
+    }));
+  };
+
   render() {
-    
     const { index, slides } = this.state;
+    const currentSlide = slides[index];
 
     return (
-      <>
-      <button id="button-restart"></button>
-      <button id="button-prev"></button>
-      <button id="button-next"></button>
-      </>
+      <div>
+        <h1 id="title">{currentSlide.title}</h1>
+        <p id="text">{currentSlide.text}</p>
+
+        <button
+          id="button-restart"
+          onClick={this.handleRestart}
+          disabled={index === 0}
+        >
+          Restart
+        </button>
+        <button
+          id="button-prev"
+          onClick={this.handlePrev}
+          disabled={index === 0}
+        >
+          Prev
+        </button>
+        <button
+          id="button-next"
+          onClick={this.handleNext}
+          disabled={index === slides.length - 1}
+        >
+          Next
+        </button>
+      </div>
     );
   }
 }
 
 export default App;
-
